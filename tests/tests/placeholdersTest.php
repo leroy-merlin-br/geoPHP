40a6aa847df19e6f35ca17cccf9bdb990f578f5d
Zizaco <zizaco@gmail.com>
2015-04-01 17:27:22 -0300

    Fixed PSR of test classes and also the directories where the input are loaded from

--- tests/tests/placeholdersTest.php
+++ tests/tests/placeholdersTest.php
@@ -1,60 +1,59 @@
 <?php
-require_once('../geoPHP.inc');
+
+use GeoPHP\GeoPHP;
+
 class PlaceholdersTests extends PHPUnit_Framework_TestCase {
 
-  function setUp() {
+    public function testPlaceholders()
+    {
+        foreach (scandir(__DIR__.'/../input') as $file) {
+            $parts = explode('.', $file);
+            if ($parts[0]) {
+                $format = $parts[1];
+                $value = file_get_contents(__DIR__.'/../input/'.$file);
+                echo "\nloading: " . $file . " for format: " . $format;
+                $geometry = GeoPHP::load($value, $format);
 
-  }
+                $placeholders = array(
+                    array('name' => 'hasZ'),
+                    array('name' => 'is3D'),
+                    array('name' => 'isMeasured'),
+                    array('name' => 'isEmpty'),
+                    array('name' => 'coordinateDimension'),
+                    array('name' => 'z'),
+                    array('name' => 'm'),
+                );
 
-  function testPlaceholders() {
-    foreach (scandir('./input') as $file) {
-      $parts = explode('.',$file);
-      if ($parts[0]) {
-        $format = $parts[1];
-        $value = file_get_contents('./input/'.$file);
-        echo "\nloading: " . $file . " for format: " . $format;
-        $geometry = geoPHP::load($value, $format);
+                foreach ($placeholders as $method) {
+                    $argument = null;
+                    $method_name = $method['name'];
+                    if (isset($method['argument'])) {
+                        $argument = $method['argument'];
+                    }
 
-        $placeholders = array(
-          array('name' => 'hasZ'),
-          array('name' => 'is3D'),
-          array('name' => 'isMeasured'),
-          array('name' => 'isEmpty'),
-          array('name' => 'coordinateDimension'),
-          array('name' => 'z'),
-          array('name' => 'm'),
-        );
+                    switch ($method_name) {
+                        case 'hasZ':
+                            if ($geometry->geometryType() == 'Point') {
+                                $this->assertNotNull($geometry->$method_name($argument), 'Failed on ' . $method_name .' (test file: ' . $file . ')');
+                            }
+                            if ($geometry->geometryType() == 'LineString') {
+                                $this->assertNotNull($geometry->$method_name($argument), 'Failed on ' . $method_name .' (test file: ' . $file . ')');
+                            }
+                            if ($geometry->geometryType() == 'MultiLineString') {
+                                $this->assertNotNull($geometry->$method_name($argument), 'Failed on ' . $method_name .' (test file: ' . $file . ')');
+                            }
+                            break;
+                        case 'm':
+                        case 'z':
+                        case 'coordinateDimension':
+                        case 'isEmpty':
+                        case 'isMeasured':
+                        case 'is3D':
+                    }
+                }
 
-        foreach($placeholders as $method) {
-          $argument = NULL;
-          $method_name = $method['name'];
-          if (isset($method['argument'])) {
-            $argument = $method['argument'];
-          }
-
-          switch ($method_name) {
-            case 'hasZ':
-              if ($geometry->geometryType() == 'Point') {
-                $this->assertNotNull($geometry->$method_name($argument), 'Failed on ' . $method_name .' (test file: ' . $file . ')');
-              }
-              if ($geometry->geometryType() == 'LineString') {
-                $this->assertNotNull($geometry->$method_name($argument), 'Failed on ' . $method_name .' (test file: ' . $file . ')');
-              }
-              if ($geometry->geometryType() == 'MultiLineString') {
-                $this->assertNotNull($geometry->$method_name($argument), 'Failed on ' . $method_name .' (test file: ' . $file . ')');
-              }
-              break;
-            case 'm':
-            case 'z':
-            case 'coordinateDimension':
-            case 'isEmpty':
-            case 'isMeasured':
-            case 'is3D':
-          }
+            }
         }
 
-      }
     }
-
-  }
 }
