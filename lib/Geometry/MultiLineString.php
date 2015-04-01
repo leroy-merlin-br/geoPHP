55438f6c40c39bd1bf2d05ed74956d38d7ce101f
Zizaco <zizaco@gmail.com>
2015-04-01 16:01:53 -0300

    Updated Geometry\LineString and MultiLineString to follow PSR-2

--- lib/Geometry/MultiLineString.php
+++ lib/Geometry/MultiLineString.php
@@ -6,17 +6,18 @@ namespace GeoPHP\Geometry;
  */
 class MultiLineString extends Collection
 {
-  protected $geom_type = 'MultiLineString';
+    protected $geom_type = 'MultiLineString';
 
-  // MultiLineString is closed if all it's components are closed
-  public function isClosed() {
-    foreach ($this->components as $line) {
-      if (!$line->isClosed()) {
-        return FALSE;
-      }
+    // MultiLineString is closed if all it's components are closed
+    public function isClosed()
+    {
+        foreach ($this->components as $line) {
+            if (!$line->isClosed()) {
+                return false;
+            }
+        }
+        return true;
     }
-    return TRUE;
-  }
 
 }
 
